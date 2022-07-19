3cc915d8f65d31769cd8262b9d3517c2498ee316
ccc <chao373737@gmail.com>
2022-07-19 11:04:50 +0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Item from "./pages/Item";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Header />
+          <Route path="/"  element={<Home/>} />
+          <Route path="/create-nft" element={<Create/>} />
+          <Route path="/nft/:nftId" element={<Item/>} />
+          <Route>404 Not Found!</Route>
+      </Router>
     </div>
   );
 }
