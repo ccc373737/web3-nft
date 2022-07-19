9c1a32e3c83426c5293c357b107e4a2dc0ce3821
ccc <chao373737@gmail.com>
2022-07-19 19:52:00 +0800

    li

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Create from "./pages/Create";
 import Item from "./pages/Item";
 import Header from "./components/Header";
+import CssBaseline from "@mui/material/CssBaseline";
+
+
+const Header11 = () => {
+  return ( 
+    
+  <React.Fragment>
+      <CssBaseline />
+      <div>hererdddddd</div>
+      <div>sdad</div>
+      </React.Fragment>)
+}
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Header />
+      <BrowserRouter>
+        <Header11 />
+        <Routes>
           <Route path="/"  element={<Home/>} />
           <Route path="/create-nft" element={<Create/>} />
           <Route path="/nft/:nftId" element={<Item/>} />
-          <Route>404 Not Found!</Route>
-      </Router>
+          <Route path="/*">404 Not Found!</Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,39 @@
 import React from "react";
+import '../../style/Global.css'
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import AppBar from "@material-ui/core/AppBar";
+import AppBar from "@mui/material/AppBar";
 
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-
-
-import {useStyles} from './styles.js'
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import Button from '@mui/material/Button';
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 import logo from '../../assets/Logo.svg';
 
 const Header = () => {
-  const classes = useStyles();
-  const account = useSelector((state) => state.allNft.account);
+  //const account = useSelector((state) => state.allNft.account);
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar className={classes.header}>
+      
+      <AppBar className="header">
         <Toolbar>
           <Link to="/">
-            <img src={logo} alt="Galerie" className={classes.logo}/>
+            <img src={logo} alt="Galerie" className="logo"/>
           </Link>
-          <div className={classes.account}>
+          {/* <div className={classes.account}>
             <AccountBalanceWalletIcon titleAccess="Wallet Address" className={classes.walletIcon}/>
             <Typography variant="subtitle1">{account.slice(0,7)}...{account.slice(-4)}</Typography>
-          </div>
+          </div> */}
         </Toolbar>
+
+        {/* <button className="square" >
+        </button> */}
       </AppBar>
-      <Toolbar />
     </React.Fragment>
   );
 };
