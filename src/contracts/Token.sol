58ae2a084c7095d91d81bddca155bda4d5ce2d54
cqc <chao343434@126.com>
2022-08-01 20:23:05 +0800

    token mint not owner

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -14,7 +14,7 @@ contract Token is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 
     constructor() ERC721("MyToken", "MTK") {}
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, string memory uri) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
